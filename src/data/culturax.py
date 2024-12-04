eb768a26f3e4897578041a3b29fba531f9df0c27
Ziqin Gong <gongzq0301@gmail.com>
2024-12-04 18:42:50 +0800

    offset for data reader

--- src/data/culturax.py
+++ src/data/culturax.py
@@ -9,9 +9,12 @@ class CulturaXData:
     df: pd.DataFrame
     texts: pd.Series
 
-    def __init__(self, parquet_file: Path) -> None:
+    def __init__(self, parquet_file: Path, offset: int = 0) -> None:
         self.df = pd.read_parquet(parquet_file)
         self.texts = self.df["text"]
+
+        if offset > 0:
+            self.texts = self.texts[offset:]
 
     def first_k(self, k: int, shuffle: bool = False) -> Iterable[str]:
         if not shuffle:
