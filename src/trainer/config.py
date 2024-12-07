32845b85cc6cc0c136facc8c0ecb8c0fa918e697
Ziqin Gong <gongzq0301@gmail.com>
2024-12-07 13:09:21 +0800

    trainer config

---
+++ src/trainer/config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class AdamConfig:
+    beta_1: float = 0.0
+    beta_2: float = 0.999
+    eps: float = 1e-8
+
+
+@dataclass
+class TrainerConfig:
+    hid_feats: int
+    sparsity_coef: float
+    batch_size: int
+    buffer_size: int  # how many activations are loaded in memory
+    use_pre_enc_bias: bool = True
+    learning_rate: float = 7e-5
+    adam: AdamConfig = field(default_factory=AdamConfig)
