ae8fe38d8d0170101549f78e4e6d9226f1594d5a
Ziqin Gong <gongzq0301@gmail.com>
2024-12-07 21:02:37 +0800

    trainer init train_state

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -2,7 +2,10 @@ import random
 from pathlib import Path
 
 import chex
+import jax.numpy as jnp
 import jax.random
+import optax as ox
+from flax.training.train_state import TrainState
 
 from src.data.sampler import DataSampler
 from src.models.sae import SparseAutoencoder
@@ -25,8 +28,39 @@ class Trainer:
         self.data_sampler: DataSampler
         self._init_data_sampler(data_dir, log_path)
 
-        self.sae: SparseAutoencoder
+        self.sae: TrainState
+        self.lmbda_scheduler: ox.Schedule
         self._init_sae()
+
+    def _init_sae(self) -> None:
+        config = self.config
+
+        sae_def = SparseAutoencoder(config.hid_feats, config.use_pre_enc_bias)
+
+        variables = sae_def.init(self._get_key(), self.data_sampler.buffer)
+
+        tx, self.lmbda_scheduler = self._init_optim(
+            config.learning_rate, config.sparsity_coef
+        )
+
+        self.sae = TrainState.create(apply_fn=sae_def.apply, params=variables, tx=tx)
+
+    def _init_optim(
+        self, learning_rate: float, sparsity_coef: float
+    ) -> tuple[ox.GradientTransformation, ox.Schedule]:
+        # Sec 3.2
+        with_warmup = ox.cosine_onecycle_schedule(
+            transition_steps=1_000,
+            peak_value=learning_rate,
+            pct_start=1,
+            div_factor=10,
+            final_div_factor=1,
+        )
+        tx = ox.adam(learning_rate=with_warmup, b1=0, b2=0.999, eps=1e-8)
+        lmbda = jnp.array(sparsity_coef, dtype=jnp.float32)
+        lmbda_scheduler = ox.linear_schedule(jnp.zeros_like(lmbda), lmbda, 10_000)
+
+        return tx, lmbda_scheduler
 
     def _init_rng(self, seed: int) -> None:
         random.seed(seed)
@@ -37,8 +71,6 @@ class Trainer:
         self.data_sampler = DataSampler(
             data_dir, config.metadata, config.batch_size, config.buffer_size, log_path
         )
-
-    def _init_sae(self) -> None: ...
 
     def _get_key(self) -> chex.PRNGKey:
         self._key, sk = jax.random.split(self._key)
