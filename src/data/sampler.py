8b4e5c61874a4ef7569687d118bca1d332fc70ae
Ziqin Gong <gongzq0301@gmail.com>
2024-12-07 15:23:34 +0800

    reset sampler

--- src/data/sampler.py
+++ src/data/sampler.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 
 import numpy as np
@@ -16,6 +17,20 @@ class DataSampler:
         self.data_files: list[Path]
         self._locate_data_files(data_dir)
 
+        self.buffer: np.ndarray
+        self.idx: int
+        self.reset()
+
+    def reset(self) -> None:
+        random.shuffle(self.data_files)
+        self.buffer = np.load(self.data_files[0])
+        self.idx = 1
+
+        while self.buffer.shape[0] < self.buffer_size and self.idx < len(self):
+            next_arr = np.load(self.data_files[self.idx])
+            self.buffer = np.concatenate([self.buffer, next_arr], axis=0)
+            self.idx += 1
+
     def _locate_data_files(self, data_dir: Path) -> None:
         if "/" in self.metadata.model_name:  # e.g. Qwen/Qwen2-7B
             model_name = self.metadata.model_name.split("/")[-1]
@@ -29,3 +44,6 @@ class DataSampler:
         data_dir = data_dir / dir_name
         glob_pattern = f"l{self.metadata.layer}-*.npy"
         self.data_files = list(data_dir.glob(glob_pattern))
+
+    def __len__(self) -> int:
+        return len(self.data_files)
