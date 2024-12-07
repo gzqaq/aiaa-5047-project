3f862dacd1cc65128600c0ce5c77c04f744b456d
Ziqin Gong <gongzq0301@gmail.com>
2024-12-07 16:30:38 +0800

    trainer init sampler

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from src.data.sampler import DataSampler
 from src.models.sae import SparseAutoencoder
 from src.trainer.config import TrainerConfig
@@ -6,15 +8,21 @@ from src.trainer.config import TrainerConfig
 class Trainer:
     config: TrainerConfig
 
-    def __init__(self, config: TrainerConfig) -> None:
+    def __init__(
+        self, config: TrainerConfig, data_dir: Path, log_path: Path | None = None
+    ) -> None:
         self.config = config
 
         self.data_sampler: DataSampler
-        self._init_data_sampler()
+        self._init_data_sampler(data_dir, log_path)
 
         self.sae: SparseAutoencoder
         self._init_sae()
 
-    def _init_data_sampler(self) -> None: ...
+    def _init_data_sampler(self, data_dir: Path, log_path: Path | None = None) -> None:
+        config = self.config
+        self.data_sampler = DataSampler(
+            data_dir, config.metadata, config.batch_size, config.buffer_size, log_path
+        )
 
     def _init_sae(self) -> None: ...
