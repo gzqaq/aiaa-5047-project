317daea0b2aaf013317d6d259bd4f7a1b99ec946
Ziqin Gong <gongzq0301@gmail.com>
2024-12-07 20:55:28 +0800

    trainer init seed

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -1,4 +1,8 @@
+import random
 from pathlib import Path
+
+import chex
+import jax.random
 
 from src.data.sampler import DataSampler
 from src.models.sae import SparseAutoencoder
@@ -6,18 +10,27 @@ from src.trainer.config import TrainerConfig
 
 
 class Trainer:
-    config: TrainerConfig
-
     def __init__(
-        self, config: TrainerConfig, data_dir: Path, log_path: Path | None = None
+        self,
+        config: TrainerConfig,
+        data_dir: Path,
+        log_path: Path | None = None,
+        seed: int = 0,
     ) -> None:
         self.config = config
+
+        self._key: chex.PRNGKey
+        self._init_rng(seed)
 
         self.data_sampler: DataSampler
         self._init_data_sampler(data_dir, log_path)
 
         self.sae: SparseAutoencoder
         self._init_sae()
+
+    def _init_rng(self, seed: int) -> None:
+        random.seed(seed)
+        self._key = jax.random.key(seed)
 
     def _init_data_sampler(self, data_dir: Path, log_path: Path | None = None) -> None:
         config = self.config
@@ -26,3 +39,7 @@ class Trainer:
         )
 
     def _init_sae(self) -> None: ...
+
+    def _get_key(self) -> chex.PRNGKey:
+        self._key, sk = jax.random.split(self._key)
+        return sk
