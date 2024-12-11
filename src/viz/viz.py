70e0f9da25a1e8245ed76f981caee138ecb9c720
Ziqin Gong <gongzq0301@gmail.com>
2024-12-11 18:31:14 +0800

    run cluster

--- src/viz/viz.py
+++ src/viz/viz.py
@@ -19,6 +19,8 @@ class Visualizer:
         log_path: Path | None = None,
     ) -> None:
         self.ckpt = sae_ckpt
+        self.learned_feats = np.array(sae_ckpt.variables["params"]["W_dec"])
+
         self.tsne_alg = TSNE(n_jobs=-1)
         self.cluster_alg = SpectralClustering(
             n_clusters=n_clusters, affinity="precomputed"
@@ -27,3 +29,15 @@ class Visualizer:
         self.co_occur = CoOccurrence(
             self.ckpt.sae_fwd, activations, chunk_size, log_path
         )
+        self.valid_feats = self.learned_feats[self.co_occur.valid_feats_mask]
+
+    def run_cluster(self, affinity_measure: str = "phi-coef") -> None:
+        if affinity_measure == "phi-coef":
+            phi_coef = self.co_occur.compute_phi_coef()
+            self.cluster_alg.fit(np.exp(phi_coef))
+        else:
+            raise NotImplementedError()
+
+        self.clusters = {}
+        for i in range(self.cluster_alg.n_clusters):
+            self.clusters[i] = self.valid_feats[self.cluster_alg.labels_ == i]
