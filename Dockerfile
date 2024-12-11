fb1593ca7b1b4df9d151133ab46642ef1ab9703b
Ziqin Gong <gongzq0301@gmail.com>
2024-12-11 17:41:09 +0800

    init tsne and spectral cluster

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,5 @@ RUN apt-get update
 RUN python -m pip install -qq \
     transformer_lens==2.9.1 \
     pandas==2.2.3 fastparquet==2024.11.0
+
+RUN python -m pip install -qq scikit-learn==1.6.0
--- src/viz/viz.py
+++ src/viz/viz.py
@@ -1,6 +1,15 @@
+from sklearn.cluster import SpectralClustering
+from sklearn.manifold import TSNE
+
 from src.utils.ckpt import SAECheckpoint
 
 
 class Visualizer:
-    def __init__(self, sae_ckpt: SAECheckpoint, n_cluster: int = 2) -> None:
+    def __init__(
+        self, sae_ckpt: SAECheckpoint, n_components: int = 2, n_clusters: int = 2
+    ) -> None:
         self.ckpt = sae_ckpt
+        self.tsne_alg = TSNE(n_jobs=-1)
+        self.cluster_alg = SpectralClustering(
+            n_clusters=n_clusters, affinity="precomputed"
+        )
