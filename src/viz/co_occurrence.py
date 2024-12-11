d53b18aa3b1e843a11fde8d0e2247e0ad0fb32ac
Ziqin Gong <gongzq0301@gmail.com>
2024-12-11 15:34:19 +0800

    set activations

--- src/viz/co_occurrence.py
+++ src/viz/co_occurrence.py
@@ -21,17 +21,26 @@ class CoOccurrence:
         self.sae_fwd = sae_fwd
         self.chunk_size = chunk_size
         self.n_chunks = activations.shape[0] // chunk_size
-
-        self.activations = activations[: self.n_chunks * chunk_size]
         self.logger.info(
             f"Will measure co-occurrence on {self.n_chunks}x{chunk_size} activations"
         )
 
-    def get_sae_act(self) -> None:
+        self.set_activations(activations)
+
+    def set_activations(self, activations: np.ndarray) -> None:
+        """Always use this method to set activations, because some post-processing will be done."""
+        self.activations = activations[: self.n_chunks * self.chunk_size]
+        self.set_sae_act()
+        self.set_chunkwise_co_occur()
+        self.set_histogram_of_all_feats()
+        self.set_histogram_of_valid_feats()
+        self.compute_help_matrices()
+
+    def set_sae_act(self) -> None:
         _, sae_act = self.sae_fwd(self.activations)
         self.sae_act = np.array(sae_act)
 
-    def get_chunkwise_co_occur(self) -> None:
+    def set_chunkwise_co_occur(self) -> None:
         co_occur_times_per_chunk = self.sae_act.reshape(
             self.n_chunks, self.chunk_size, -1
         ).sum(1)
@@ -40,11 +49,11 @@ class CoOccurrence:
             co_occur_per_chunk_p, co_occur_times_per_chunk.dtype
         )
 
-    def get_histogram_of_all_feats(self) -> None:
+    def set_histogram_of_all_feats(self) -> None:
         """Compute the co-occurrence histogram of all learned features."""
         self.hist_of_all = self.chunkwise_co_occur.T @ self.chunkwise_co_occur
 
-    def get_histogram_of_valid_feats(self) -> None:
+    def set_histogram_of_valid_feats(self) -> None:
         """
         Compute the co-occurrence histogram of features that occur and don't occur at least once.
         """
