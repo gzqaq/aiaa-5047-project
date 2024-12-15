32db177cde7f1f1751016b4e8f72a6b7b53b16a0
Ziqin Gong <gongzq0301@gmail.com>
2024-12-15 18:22:02 +0800

    cache loggers

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -3,9 +3,17 @@ from logging import (DEBUG, INFO, FileHandler, Formatter, Logger,
 from pathlib import Path
 
 DEFAULT_LOG_PATH = Path("~/.cache/").expanduser() / "aiaa-5047-project.log"
+LOGGER_CACHE: dict[str, Logger] = {}
 
 
 def setup_logger(name: str, log_path: Path | None = None) -> Logger:
+    global LOGGER_CACHE
+
+    if name in LOGGER_CACHE:
+        logger = LOGGER_CACHE[name]
+        logger.info(f"Reuse the same logger named {name}")
+        return logger
+
     if log_path is None:
         log_path = DEFAULT_LOG_PATH
 
@@ -25,5 +33,6 @@ def setup_logger(name: str, log_path: Path | None = None) -> Logger:
     logger.addHandler(stream_handler)
 
     logger.info(f"Set up a logger named {name}, log file located at {log_path}")
+    LOGGER_CACHE[name] = logger
 
     return logger
